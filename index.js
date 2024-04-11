8ec650f6c5dfa7c261fd33aa361d67a987793374
Richard Job <richardjob05@gmail.com>
2024-04-11 22:08:44 +0530

    updated

--- index.js
+++ index.js
@@ -108,5 +108,5 @@ async function getAllItemsFromFeed(feed) {
 }
 
 app.listen(process.env.PORT, ()=>{
-  console.log("Server Started at PORT: " + process.env.PORT);
+  console.log("Server started at PORT: " + process.env.PORT);
 })
